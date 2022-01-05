1d8b18a7ae0d4672643c223f20e6f1826c525fa0
Roque Francisco <roquefsd@gmail.com>
2022-01-04 21:15:54 -0300

    v1.1.0

--- Assets/DrawSplines.cs
+++ Assets/DrawSplines.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+#if UNITY_EDITOR
 [ExecuteInEditMode]
-public class DrawSplines : MonoBehaviour
-{
+public class DrawSplines : MonoBehaviour {
     public Color color = Color.red;
     public bool enabled = true;
+    public float size = .05f;
 
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos() {
         if(enabled == true) {
             for(int i = 0; i < this.transform.childCount; i++)
             {
@@ -22,15 +22,23 @@ public class DrawSplines : MonoBehaviour
                     if (lastSibling.x != 0) {
                         DrawLine(lastSibling, Sibling.transform.position);
                     }
+                    DrawBox(Sibling.transform.position, n);
                     lastSibling = Sibling.transform.position;
                 }
             }
         }
     }
 
-    private void DrawLine(Vector3 start, Vector3 end)
-    {
+    private void DrawBox(Vector3 pos, int i) {
+        Handles.color = color;
         Gizmos.color = color;
-        Gizmos.DrawLine(start, end);
+        Handles.DrawWireCube(pos, new Vector3(size, size, size));
+        Gizmos.DrawCube(pos, new Vector3(size + .01f, size + .01f, size + .01f));
+    }
+
+    private void DrawLine(Vector3 start, Vector3 end) {
+        Handles.color = color;
+        Handles.DrawLine(start, end);
     }
 }
+#endif
